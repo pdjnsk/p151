be9da400e0753c2b2e891dcd2d7dae0c489544a2
pdjnsk <80638504+pdjnsk@users.noreply.github.com>
2023-05-28 12:55:49 +0530

    Update Car.js

--- p151/js/Car.js
+++ p151/js/Car.js
@@ -3,7 +3,6 @@ AFRAME.registerComponent("car-model", {
     modelRef: { type: "string", default: "../assets/car/scene.gltf" }
   },
   init: function() {
-    // Do something when component first attached.
     this.el.setAttribute("gltf-model", this.data.modelRef);
     const position = { x: 0, y: 50, z: 80 };
     const rotation = { x: 0, y: -100, z: 0 };
